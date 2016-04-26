038b6fd1639d4e4da9c33a21920700e18690a646
marinacor1 <marinaspam1234@gmail.com>
2016-04-26 10:57:33 -0600

    Selena song lyrics appear

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
 
  $("#justin").on("click", function() {
-  $("#baby").toggle("hidden");
+  $("#baby").toggleClass("hidden");
   var counter = parseInt($("#baby").find("#view-counter").text())
   counter++
-  ("#baby").find("#view-counter").text(counter)
+  $("#baby").find("#view-counter").text(counter)
  })
+
+  $("#selena").on("click", function() {
+    $("#come-and-get-it").toggleClass("hidden");
+    var counter = parseInt($("#come-and-get-it").find("#view-counter").text())
+    counter ++
+    $("#come-and-get-it").find("#view-counter").text(counter)
+  })
+
+  $("#miley").on("click", function(){
+    $("")
+  })
 })
